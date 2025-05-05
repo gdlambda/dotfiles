1208df6ecf3dc82d0dcad31ad9593166b0f2ea31
gdlambda <gadl14@proton.me>
2025-05-05 20:31:38 +0200

    important configs

---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,19 @@
+local keymap = vim.keymap
+
+local opts = { noremap = true, silent = true } 
+
+-- Directory Navigation
+keymap.set("n", "<leader>m", ":NvimTreeFocus<CR>", opts) 
+keymap.set("n", "<leader>f", ":NvimTreeToggle<CR>", opts)
+
+-- Pane Navigation
+keymap.set("n", "<C-h>", "<C-w>h", opts)
+keymap.set("n", "<C-j>", "<C-w>j", opts)
+keymap.set("n", "<C-k>", "<C-w>k", opts)
+keymap.set("n", "<C-l>", "<C-w>l", opts)
+
+-- Window Management
+keymap.set("n", "<leader>sv", ":vsplit<CR>", opts) -- Vertical
+keymap.set("n", "<leader>sh", ":split<CR>", opts) -- Horizontal
+keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>", opts) -- Toggle Minimum
+
---
+++ nvim/lua/plugins/nvim-tree.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "nvim-tree/nvim-tree.lua",
+    lazy = false,
+    config = {
+      filters = {
+        dotfiles = true
+      }
+    }
+  },
+  {
+    "nvim-tree/nvim-web-devicons",
+  }
+}
